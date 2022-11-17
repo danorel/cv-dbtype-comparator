3669a855c8d6a40697844a754d41e0e7043223f0
danorel <mail.ordan@gmail.com>
2022-11-17 18:34:39 +0200

    Add neo4j full graph initialization/removal

--- neo4j/main.py
+++ neo4j/main.py
@@ -1,14 +1,93 @@
 from core.server import app
-from core.db.generator import Neo4jGenerator, Schema
+from core.db.generator import Neo4jGenerator
 from core.db.connector import Neo4jConnection
+
+
+Schema = {
+    'name': 'Root',
+    'properties': [
+        {
+            'name': 'User',
+            'min_items': 1000,
+            'max_items': 1000,
+            'properties': [
+                {
+                    'name': 'login',
+                    'instance': 'str',
+                    'range_from': 8,
+                    'range_to': 8
+                },
+                {
+                    'name': 'password',
+                    'instance': 'str',
+                    'range_from': 8,
+                    'range_to': 16
+                },
+                {
+                    'name': 'CV',
+                    'min_items': 1,
+                    'max_items': 5,
+                    'properties': [
+                        {
+                            'name': 'title',
+                            'instance': 'str',
+                            'range_from': 5,
+                            'range_to': 15
+                        },
+                        {
+                            'name': 'Hobby',
+                            'min_items': 1,
+                            'max_items': 5,
+                            'properties': [
+                                {
+                                    'name': 'title',
+                                    'instance': 'str',
+                                    'range_from': 3,
+                                    'range_to': 15
+                                }
+                            ]
+                        },
+                        {
+                            'name': 'Company',
+                            'min_items': 1,
+                            'max_items': 3,
+                            'properties': [
+                                {
+                                    'name': 'title',
+                                    'instance': 'str',
+                                    'range_from': 3,
+                                    'range_to': 15
+                                },
+                                {
+                                    'name': 'City',
+                                    'min_items': 1,
+                                    'max_items': 2,
+                                    'properties': [
+                                        {
+                                            'name': 'title',
+                                            'instance': 'str',
+                                            'range_from': 5,
+                                            'range_to': 15,
+                                            'cache': True
+                                        }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
 
 
 def Neo4jInitializer(connection: Neo4jConnection, generator: Neo4jGenerator):
     root = generator.random(Schema)
 
-    users = root.get('User')
+    users = root.get('User', [])
     for user in users:
-        cvs = user.get('CV')
+        cvs = user.get('CV', [])
         for cv in cvs:
             connection.query(f"""
                 CREATE
@@ -21,6 +100,46 @@ def Neo4jInitializer(connection: Neo4jConnection, generator: Neo4jGenerator):
                     title: \"{cv.get('title')}\"
                 }})
             """)
+            hobbies = cv.get('Hobby', [])
+            for hobby in hobbies:
+                connection.query(f"""
+                    CREATE
+                    (p:CV{{
+                        title: \"{cv.get('title')}\"
+                    }})
+                    -[:HAS]->
+                    (t:Hobby{{
+                        title: \"{hobby.get('title')}\"
+                    }})
+                """)
+            companies = cv.get('Company', [])
+            for company in companies:
+                connection.query(f"""
+                    CREATE
+                    (p:CV{{
+                        title: \"{cv.get('title')}\"
+                    }})
+                    -[:HAS]->
+                    (t:Company{{
+                        title: \"{company.get('title')}\"
+                    }})
+                """)
+                cities = company.get('City', [])
+                for city in cities:
+                    connection.query(f"""
+                        CREATE
+                        (p:Company{{
+                            title: \"{company.get('title')}\"
+                        }})
+                        -[:IN]->
+                        (t:City{{
+                            title: \"{city.get('title')}\"
+                        }})
+                    """)
+
+
+def Neo4jRemover(connection: Neo4jConnection):
+    connection.query("""MATCH (n) DETACH DELETE n""")
 
 
 def main():
@@ -29,8 +148,8 @@ def main():
                                      user="neo4j",
                                      pwd="supersecretpassword")
         generator = Neo4jGenerator()
-        Neo4jInitializer(connection, generator)
-        # connection.query("""MATCH (n) DETACH DELETE n""")
+        # Neo4jInitializer(connection, generator)
+        # Neo4jRemover(connection)
         connection.close()
     except Exception as e:
         return f"Error. {e}"
